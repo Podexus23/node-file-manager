2d71e102a2c5510fd55fa7d4def2ad9d98b335e4
Podexus23 <santon23@gmail.com>
2025-11-03 00:12:28 +0300

    feat: implemented initial app logic

---
+++ app.js
@@ -0,0 +1,35 @@
+import { stdin } from "node:process";
+import {
+  printGoodBye,
+  printHello,
+  printWorkingDirectory,
+} from "./modules/printOps.js";
+
+import { setUserDir, setUserName } from "./modules/helpers.js";
+
+export const userData = {
+  userName: "Username",
+  currentDir: "",
+};
+
+function main() {
+  //start program
+  setUserName(userData);
+  printHello(userData.userName);
+  setUserDir(userData);
+  printWorkingDirectory(userData.currentDir);
+
+  //take data from terminal
+  stdin.setEncoding("utf-8");
+  stdin.on("data", (data) => {
+    console.log(data);
+  });
+
+  //exit on ctrl+c
+  process.on("SIGINT", (signal) => {
+    printGoodBye(userData.userName);
+    process.exit(0);
+  });
+}
+
+main();
